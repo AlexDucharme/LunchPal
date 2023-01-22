45085492e114a20eebcba79a8d20112ef78c92bb
Alex <64511464+KeenCodeDrop@users.noreply.github.com>
2023-01-21 22:45:52 -0500

    Update LunchPal_CLI.py
    Fixing some small bug

--- LunchPal_CLI.py
+++ LunchPal_CLI.py
@@ -1,8 +1,10 @@
-from LunchPal import *
-import subprocess
-import os, sys
+import sys
+import os
+import time
+import mido
+import pickle
 
-sys.path.append(str(os.getcwd()+"/Algorithms"))
+##########################################################################################
 '''
    #---------------------------------#
         ╦  ┬ ┬┌┐┌┌─┐┬ ┬╔═╗┌─┐┬  
@@ -10,118 +12,194 @@ sys.path.append(str(os.getcwd()+"/Algorithms"))
         ╩═╝└─┘┘└┘└─┘┴ ┴╩  ┴ ┴┴─┘               
     #-------------------------------#
 Becasue MIDI == Lunch Time !
-Command Line Interface to create, view and summon LunchPal
-If your are not using gnome terminale your should edit the TERMINAL variable
-'''
-# DEFAULT TERMINAL TOOL
-TERMINAL = "gnome-terminal"
-
-# BASIC MENU
-MENU = '''
-       [1] - Sketch a new LunchPal
-       [2] - List all LunchPal 
-       [3] - List all Algorithms
-       [!] - Summon a LunchPal
-       [q] - QUIT or Ctrl+c
+LunchPal class to create object that are pass to MIDI procesing algorithm.
+You can setup, save and load a LunchPal with the method setupLunchPal(), saveLunchPal()
+and loadLunchPal().
+You can display the LunchPal attribute with the LunchPalInfo() method.
+At creation, the atributes of a LunchPal object are all empty.
 '''
 
-def printMainMenu():
-    print(LOGO)
-    print(MENU)
+LOGO = '''           #---------------------------------#
+                ╦  ┬ ┬┌┐┌┌─┐┬ ┬╔═╗┌─┐┬  
+                ║  │ │││││  ├─┤╠═╝├─┤│  
+                ╩═╝└─┘┘└┘└─┘┴ ┴╩  ┴ ┴┴─┘               
+            #-------------------------------#'''
 
-def listAlgorithme():
-    print("\n - - ALGORITHMES - -")
-    for i in getAlgoList():
-        print("--> "+i)
-    print("- - - - - - - - - - -")
-    press_ent = input('Press Enter to continue!')
+class LunchPal(object):
 
-def newLunchPal():
-    newPal = LunchPal()
-    newPal.setupLunchPal()
-    newPal.saveLunchPal()
-
-def summonLunchPal():
-    algoPath = str(os.getcwd()+"/Algorithms")
-    selectedLunchPal = selectLunchPal()
-    selectedAlgorithm = selectAlgorithme()
-    os.chdir(algoPath) # Change path to summon
-    cmd = [TERMINAL, "--", "python3", selectedAlgorithm, selectedLunchPal]
-    subprocess.run(cmd)
-    os.chdir("..") # Come back
+        def __init__(self):
+            self.NAME = ""
+            self.NBR_SOURCE_IN = 0
+            self.INPUTS = {}
+            self.NBR_SOURCE_OUT = 0
+            self.OUTPUTS = {}
 
 
-def main():
-    main_loop = True
-    while main_loop:
-        printMainMenu()
-        user_input = input('[Option] : ')
-        if user_input not in ['1', '2', '3', '!', 'q']:
-            pass
-        else:
-            if user_input == '1':
-                newLunchPal()
-            elif user_input == '2':
-                printListLunchPal()
-                press_ent = input('Press Enter to continue!')
-            elif user_input == '3':
-                listAlgorithme()
-            elif user_input == '!':
-                summonLunchPal()
-            elif user_input == 'q':
-                main_loop = False
+        def setName(self):
+            self.NAME = input("Enter a name for your new LunchPal :")
+#
+        def setSource(self, inOut):
+            nbr_source = input("How many "+inOut+" source ? : ")
+            if nbr_source in "1234567890":
+                if inOut == "input":
+                    self.NBR_SOURCE_IN = nbr_source
+                elif inOut == "output":
+                    self.NBR_SOURCE_OUT = nbr_source
+                if nbr_source == 0:
+                    if inOut == "input":
+                        self.INPUTS["NON"] = []
+                    elif inOut == "output":
+                        self.OUTPUTS["NON"] = []
+                #Initialisation of sources dictionary, at first it was for channel
+                #but I disable sont channel feature for now
+                else:
+                    for i in range(0, int(nbr_source)):
+                        if inOut == "input":
+                            source = self.chooseSource(inOut)
+                            self.INPUTS[source] = []
+                        elif inOut == "output":
+                            source = self.chooseSource(inOut)
+                            self.OUTPUTS[source] = []
+            else :
+                print("[!] - Invalid entry !")
+                self.setSource(str(inOut))
 
-def printListLunchPal():
-    lchpal_list = []
-    for f in os.listdir(str(os.getcwd()+"/Pals")):
-        try:
-            ext = f.split('.')[1]
-            if ext == "lchPal":
-                lchpal_list.append(f)
-        except Exception as e:
-            pass
-
-    for pal in lchpal_list:
-        print("[*] "+pal)
-
-
-def selectLunchPal():
-    lchpal_list = []
-    for f in os.listdir(str(os.getcwd()+"/Pals/")):
-        try:
-            ext = f.split('.')[1]
-            if ext == "lchPal":
-                lchpal_list.append(f)
-        except Exception as e:
-            pass
-
-    xx = 1    
-    print("Choose LunchPal :")
-    for pal in lchpal_list:
-        print("["+str(xx)+"] - "+pal)
-        xx += 1
-    selectedLunchPal = input('[ --> ]  ')
-    return lchpal_list[int(selectedLunchPal)-1]
-
-
-def getAlgoList():
-    algoList = []
-    for f in os.listdir(str(os.getcwd()+"/Algorithms")):
-        f.split('.')[0]
-        algoList.append(f)
-    return algoList
-
-
-def selectAlgorithme():
-    algoList = getAlgoList()
-    xx = 1
-    print("\nChoose Algorithm :")
-    for algo in algoList:
-        print("["+str(xx)+"] - "+str(algo))
-        xx += 1
-    selectedAlgo = input('[ --> ]  ')
-    return algoList[int(selectedAlgo)-1]
+        # Should be define in the algorythm for a better fluidité
+        # def setChannel(self, inOut):
+        #     if inOut == "input":
+        #         for k in self.INPUTS:
+        #             print("Enter "+inOut+" channel for "+k+", separeted by coma.")
+        #             print("EXEMPLE : 2,3,4,6")
+        #             channels = input('[ --> ]  ')
+        #             self.INPUTS[k] = channels.split(",")
+        #     elif inOut == "output":
+        #         for k in self.OUTPUTS:
+        #             print("Enter "+inOut+" channel for "+k+", separeted by coma.")
+        #             print("EXEMPLE : 2,3,4,6")
+        #             channels = input('[ --> ]  ')
+        #             self.OUTPUTS[k] = channels.split(",")
+        
+        #Channel selections when creating a pal is disable
+        def setupLunchPal(self):
+            self.NAME = input("MIDI Friend name [ --> ]  ")
+            self.setSource("input")
+            # if self.NBR_SOURCE_IN != 0:
+            #     self.setChannel("input")
+            # else:
+            #     self.INPUTS["NON"] = []
+            
+            self.setSource("output")
+            # if self.NBR_SOURCE_OUT != 0:
+            #     self.setChannel("output")
+            # else:
+            #     self.OUTPUTS["NON"] = []
 
 
+        def saveLunchPal(self):
+            try:
+                pickle.dump(self, open("Pals/"+self.NAME+".lchPal", "wb" ))
+            except :
+                pickle.dump(self, open("Pals/"+self.NAME+".lchPal", "w" ))
+
+        def loadLunchPal(self, LunchPalName):
+            tmp_self = pickle.load(open("../Pals/"+LunchPalName, "rb" ))
+            self.NAME = tmp_self.NAME
+            self.NBR_SOURCE_IN = tmp_self.NBR_SOURCE_IN
+            self.INPUTS = tmp_self.INPUTS
+            self.NBR_SOURCE_OUT = tmp_self.NBR_SOURCE_OUT
+            self.OUTPUTS = tmp_self.OUTPUTS
+
+        
+        def LunchPalInfo(self):
+            print(LOGO)
+            print("")
+            print("[NAME] : "+self.NAME)
+            for k in self.OUTPUTS:
+                print("[MIDI OUTPUT] : "+str(k))
+            for k in self.INPUTS:
+                #print("[!] MIDI INPUT : \n[*] - "+str(k)+" - CHANNEL(S): "+str(self.INPUTS[k]))
+                print("[MIDI INPUT] : "+str(k))
+            print("\n                 #---------------------#")
+
+
+        #Function use to select MIDI sources, IN or OUT
+        def chooseSource(self, inOut):
+            # Variable settings
+            if inOut == "input":
+                source = "input"
+                nbr_source_device = len(mido.get_input_names())
+                device_list = mido.get_input_names()
+            elif inOut == "output":
+                source = "output"
+                nbr_source_device = len(mido.get_output_names())
+                device_list = mido.get_output_names()
+            #--------------------   
+            print("[ "+self.NAME+" ] - -  CHOOSE "+source.upper()+" SOURCE(S) - - ")
+            #--------------------
+            # Display all sources
+            xx = 1   # Counter, ID
+            for device in device_list :
+                print("["+str(xx)+"] " + device)
+                xx = xx + 1
+            print("["+str(xx)+"] NON")
+            #--------------------
+            #Choice of input (User will input source ID number)
+            CHOICE= input('[ --> ]  ')
+            print(CHOICE)
+            if CHOICE in "1234567890" and int(CHOICE) <= xx:
+                try:
+                    if (int(CHOICE) <= nbr_source_device):
+                        CHOICE = device_list[int(CHOICE)-1]
+                        return CHOICE
+                    elif (int(CHOICE) == xx) :
+                        return "NON"
+                    else:
+                        print("\n[!] - No "+source+" port selected")
+                except Exception as e:
+                    print(e)
+            else :
+                print("[!] - Invalid entry !")
+                self.chooseSource(str(inOut))
+
+
+        # Methode to summon a LunchPal
+        def summon(self, algorithmeName, func):
+            try:
+                print("\n[~~~] - Lunching "+str(algorithmeName).upper()+" algorithm ...")
+                return func(self) 
+            except Exception as e:
+                print("Error lunching algorithm, sorry about that ... .. .")
+                print(e)
+                pass           
+
+
+##################################################################################################################
+##################################################################################################################
+'''
+▓█████▄ ▓█████  ▄████▄   ▒█████   ██▀███   ▄▄▄     ▄▄▄█████▓ ▒█████   ██▀███  
+▒██▀ ██▌▓█   ▀ ▒██▀ ▀█  ▒██▒  ██▒▓██ ▒ ██▒▒████▄   ▓  ██▒ ▓▒▒██▒  ██▒▓██ ▒ ██▒
+░██   █▌▒███   ▒▓█    ▄ ▒██░  ██▒▓██ ░▄█ ▒▒██  ▀█▄ ▒ ▓██░ ▒░▒██░  ██▒▓██ ░▄█ ▒
+░▓█▄   ▌▒▓█  ▄ ▒▓▓▄ ▄██▒▒██   ██░▒██▀▀█▄  ░██▄▄▄▄██░ ▓██▓ ░ ▒██   ██░▒██▀▀█▄  
+░▒████▓ ░▒████▒▒ ▓███▀ ░░ ████▓▒░░██▓ ▒██▒ ▓█   ▓██▒ ▒██▒ ░ ░ ████▓▒░░██▓ ▒██▒
+ ▒▒▓  ▒ ░░ ▒░ ░░ ░▒ ▒  ░░ ▒░▒░▒░ ░ ▒▓ ░▒▓░ ▒▒   ▓▒█░ ▒ ░░   ░ ▒░▒░▒░ ░ ▒▓ ░▒▓░
+ ░ ▒  ▒  ░ ░  ░  ░  ▒     ░ ▒ ▒░   ░▒ ░ ▒░  ▒   ▒▒ ░   ░      ░ ▒ ▒░   ░▒ ░ ▒░
+ ░ ░  ░    ░   ░        ░ ░ ░ ▒    ░░   ░   ░   ▒    ░      ░ ░ ░ ▒    ░░   ░ 
+   ░       ░  ░░ ░          ░ ░     ░           ░  ░            ░ ░     ░     
+ ░             ░                                                              
+Decorator function use for quickly prototyping MIDI processing algorithm
+'''
+
+def Algorithmz(func):
+    def inner(LunchPalName):
+        Pal = LunchPal()
+        Pal.loadLunchPal(LunchPalName)
+        Pal.LunchPalInfo()
+        Pal.summon(sys.argv[0], func)  
+    return inner
+
+
+
+##################################################################################################################
+# Stuff to do when imported
 if __name__ == '__main__':
-    main()
+    pass
